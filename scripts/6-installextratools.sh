77ac553bdf1f66c13a3bca92c6a835dfa7bc6640
russellhamker <russell@guarddogsolutions.com>
2022-06-08 17:26:05 -0500

    fix yq issues

--- scripts/6-installextratools.sh
+++ scripts/6-installextratools.sh
@@ -14,9 +14,11 @@ imgpkg version
 
 #Install yq. The YAML, JSON, and XML Processor Utility
 echo "> Installing yq. The YAML, JSON, and XML Processor Utility..."
-export VERSION=$(curl -s https://github.com/mikefarah/yq/releases/latest | cut -d '/' -f8 | cut -d '"' -f 1)
+#export VERSION=$(curl -s https://github.com/mikefarah/yq/releases/latest | cut -d '/' -f8 | cut -d '"' -f 1)
+VERSION="v4.25.2"
 BINARY=yq_linux_amd64
 sudo wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz -O - | tar xz && sudo mv ${BINARY} /usr/bin/yq
+yq --version
 
 #Download Kubecolor
 echo "> Downloading Kubecolor..."
@@ -40,6 +42,7 @@ sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 echo "> Cleaning up Directory..."
 sudo rm *
 
+<<com
 #Install K9s
 echo "> Installing K9s Utility..."
 sudo snap install k9s
@@ -47,6 +50,7 @@ sudo snap install k9s
 #Validate K9s
 echo "> Validating K9s version..."
 k9s version
+com
 
 #Set up Kubecolor permanent Alias
 echo  '> Setting up Kubecolor permanent alias...'
