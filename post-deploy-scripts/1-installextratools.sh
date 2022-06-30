bc6f6e1224b96b7dae876e80c6882f5b3c14fc49
Russell Hamker <russell@guarddogsolutions.com>
2022-06-30 16:04:36 -0600

    update output

--- post-deploy-scripts/1-installextratools.sh
+++ post-deploy-scripts/1-installextratools.sh
@@ -89,7 +89,7 @@ echo "   Installing kubectl krew ..."
   ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
   KREW="krew-${OS}_${ARCH}" &&
   curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
-  tar zxvf "${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" > /dev/null 2>&1 &&
   ./"${KREW}" install krew > /dev/null 2>&1
 )
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
