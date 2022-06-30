82bc6f55309b80944dd3f2614883065561b8a63a
Russell Hamker <russell@guarddogsolutions.com>
2022-06-30 09:38:14 -0600

    Add knative, kpack, krew, and tree utilities

--- scripts/6-installextratools.sh
+++ scripts/6-installextratools.sh
@@ -58,3 +58,46 @@ chmod 700 get_helm.sh
 ./get_helm.sh
 helm version
 rm get_helm.sh
+
+#Install Knative Client
+#https://github.com/knative/client/releases
+echo "> Installing Knative Client..."
+export VERSION=$(git ls-remote --tags https://github.com/knative/client | cut -d/ -f3- | grep knative | tail -n1 | cut -d '^' -f 1 | cut -d 'v' -f 3)
+curl -LO https://github.com/knative/client/releases/download/knative-v${VERSION}/kn-linux-amd64
+mv kn-linux-amd64 kn
+chmod +x kn
+mv kn /usr/local/bin/.
+kn version
+
+#Install Kpack Cli Client
+#https://github.com/vmware-tanzu/kpack-cli/releases
+echo "> Installing Kpack Cli Client..."
+export VERSION=$(git ls-remote --tags https://github.com/vmware-tanzu/kpack-cli | cut -d/ -f3- | tail -n1 | cut -d '^' -f 1  | cut -d 'v' -f 2)
+curl -LO https://github.com/vmware-tanzu/kpack-cli/releases/download/v${VERSION}/kp-linux-${VERSION}
+mv kp-linux-${VERSION} kp
+chmod +x kp
+mv kp /usr/local/bin/.
+kp version
+
+#Install Krew
+#https://krew.sigs.k8s.io/docs/user-guide/setup/install/
+echo "> Installing kubectl krew..."
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >> /etc/profile
+kubectl krew version
+
+#Install Tree
+#https://github.com/ahmetb/kubectl-tree
+echo "> Installing Kubectl tree..."
+kubectl krew install tree
+kubectl tree --version
+
