63cbd416d7f990c7f3eadc06d95507cecc12d725
Russell Hamker <russell@guarddogsolutions.com>
2022-06-30 19:08:51 -0600

    update rear installer

--- post-deploy-scripts/2-installrear.sh
+++ post-deploy-scripts/2-installrear.sh
@@ -16,11 +16,30 @@ git
 echo '   Installing Relax-and-Recover (rear) ...'
 git clone https://github.com/rear/rear.git
 cd rear
-sudo make install
+make install
 
 # Install prereqs
 echo '   Review rear version ...'
 rear --version
+
+# Create rear etc file
+cat <<EOF > /etc/rear/site.conf
+# Site config file for Relax and Recover (ReaR)
+
+BACKUP=NETFS
+OUTPUT=ISO
+ISO_DEFAULT="automatic"
+
+# Save to ISO
+BACKUP_URL="iso:///backup"
+OUTPUT_URL=null
+
+# Save to NFS and boot from small ISO
+# BACKUP_URL=nfs://172.26.4.30/mnt/FREENAS01/exports
+
+# Notes
+# Place file in /etc/rear/site.conf
+EOF
 
 # Clean up git clone
 echo '   Clearning Up Rear Install ...'
