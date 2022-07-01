91b11c81e57e86f0fbab11ecf1a8259bb8a9c363
Russell Hamker <russell@guarddogsolutions.com>
2022-07-01 09:15:19 -0600

    add validation for cluster build

--- automation/5-Make-Unmanaged-Cluster-and-Deploy-Velero.sh
+++ automation/5-Make-Unmanaged-Cluster-and-Deploy-Velero.sh
@@ -9,6 +9,19 @@ VELERO_PACKAGE_VERSION=1.8.0
 echo '    Creating Unmanaged Cluster ...'
 tanzu um create local-cluster -p 80:80 -p 443:443 -c calico
 
+# Valideate Cluster is ready
+echo "   Validating Unmanaged Cluster $CLUSTER_NAME is Ready ..."
+STATUS=NotReady
+while [[ $STATUS != "Ready" ]]
+do
+echo "    Tanzu Cluster $CLUSTER_NAME Status - NotReady"
+sleep 10s
+STATUS=$(kubectl get nodes -n $CONTROL_PLANE | tail -n +2 | awk '{print $2}')
+done
+echo "    Tanzu Cluster $CLUSTER_NAME Status - Ready"
+kubectl get nodes,po -A
+sleep 20s
+
 # Install Velero
 echo '    Installing Velero ...'
 # https://tanzucommunityedition.io/docs/v0.12/package-readme-cert-manager-1.8.0/
