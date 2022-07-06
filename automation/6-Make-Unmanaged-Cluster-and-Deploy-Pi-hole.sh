5134f87df197c85966e8c526679240dea42ff1fe
Russell Hamker <russell@guarddogsolutions.com>
2022-07-06 15:38:53 -0600

    add validation for pihole pad ready state

--- automation/6-Make-Unmanaged-Cluster-and-Deploy-Pi-hole.sh
+++ automation/6-Make-Unmanaged-Cluster-and-Deploy-Pi-hole.sh
@@ -176,10 +176,16 @@ echo "   Using helm to install pihole ..."
 helm install pihole mojo2600/pihole \
   --namespace pihole \
   --values pihole.values.yaml \
-  --replace \
+  --replace 
 
 kubectl get pods -n pihole -o wide
 kubectl get services -n pihole -o wide
+
+# Validate that pihole pod is ready
+echo "   Validate that pihole pod is ready ..."
+PIHOLEPOD=$(kubectl get po -n pihole | grep pihole | cut -d " " -f 1)
+while [[ $(kubectl get po -n pihole $PIHOLEPOD -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "Waiting for pod $PIHOLEPOD to be ready" && sleep 1; done
+
 
 # Create Pihole ingress rules
 echo "   Creating pihole ingress rules ..."
