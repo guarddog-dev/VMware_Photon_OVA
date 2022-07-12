61d5353b62fe4a3daa067353cd8588d15f9a6eba
Russell Hamker <russell@guarddogsolutions.com>
2022-07-12 13:20:01 -0600

    update script to allow for DNS forwarding

--- automation/6-Make-Unmanaged-Cluster-and-Deploy-Pi-hole.sh
+++ automation/6-Make-Unmanaged-Cluster-and-Deploy-Pi-hole.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-# Setup Pi-Hole Using Contour
+# Setup Pi-Hole Using Metallb
 echo '  Preparing for Pi-Hole ...'
 
 # Versions
-#Version of Cert Manager to install
-CERT_MANAGER_PACKAGE_VERSION=1.8.0
-#Version of Contour/Envoy to install
-CONTOUR_PACKAGE_VERSION=1.20.1
+#Version of MetalLB Version to install
+METALLBVERSION=0.12.1
 #Internal Domain name
 DOMAIN_NAME=$(hostname -d)
 #Internal DNS Entry to that resolves to the Pi-hole fqdn - you must make this DNS Entry
@@ -20,13 +18,18 @@ CONTROL_PLANE="$CLUSTER_NAME"-control-plane
 #Location Name
 USERD=root
 # Github Variables for YAML files
-REPO="https://github.com/guarddog-dev"
-REPONAME="VMware_Photon_OVA"
-REPOFOLDER="yaml"
+#REPO="https://github.com/guarddog-dev"
+#REPONAME="VMware_Photon_OVA"
+#REPOFOLDER="yaml"
+
+# Disable DNS Resolver on system
+echo '   Disabling system-d resolved service ...'
+sudo systemctl disable systemd-resolved
+sudo systemctl stop systemd-resolved
 
 # Create Unmanaged Cluster
 echo '   Creating Unmanaged Cluster ...'
-tanzu um create $CLUSTER_NAME -p 80:80 -p 443:443 -c calico
+tanzu um create ${CLUSTER_NAME} -c calico -p 80:80 -p 443:443 -p 53:53 -p 67:67
 
 # Valideate Cluster is ready
 echo "   Validating Unmanaged Cluster $CLUSTER_NAME is Ready ..."
@@ -41,114 +44,11 @@ echo "    Tanzu Cluster $CLUSTER_NAME Status - Ready"
 kubectl get nodes,po -A
 sleep 20s
 
-# Install Cert Manager
-echo "   Installing Cert Manager version ${CERT_MANAGER_PACKAGE_VERSION}..."
-# https://tanzucommunityedition.io/docs/v0.12/package-readme-cert-manager-1.8.0/
-tanzu package available list cert-manager.community.tanzu.vmware.com
-tanzu package install cert-manager --package-name cert-manager.community.tanzu.vmware.com --version ${CERT_MANAGER_PACKAGE_VERSION}
-tanzu package installed list -n default
-
-#Validate Package is Running
-PNAME="Cert Manager"
-PACKAGE="cert-manager"
-CSTATUS='NotRunning'
-echo "   Validating $PNAME is ready ..."
-while [[ $CSTATUS != "Running" ]]
-do
-echo "    $PNAME - NotRunning"
-APPNAME=$(kubectl -n $PACKAGE get po -l app=$PACKAGE -o name | cut -d '/' -f 2)
-CSTATUS=$(kubectl get po -n $PACKAGE | grep $APPNAME | awk '{print $3}')
-done
-echo "$PNAME - $CSTATUS"
-kubectl get po -n $PACKAGE | grep $APPNAME
-
-#Validate Tanzu Package is reconciled
-PNAME="Cert Manager"
-PACKAGE="cert-manager"
-CSTATUS='NotReconziled'
-echo "> Validating $PNAME is reconciled..."
-while [[ $CSTATUS != "Reconcile succeeded" ]]
-do
-echo "    $PNAME not reconciled"
-CSTATUS=$(tanzu package installed get $PACKAGE | grep STATUS | awk '{print $2" "$3}')
-done
-echo "$PNAME $CSTATUS"
-tanzu package installed get $PACKAGE | grep STATUS
-sleep 20s
-
-# Install Contour
-echo "   Installing Contour version ${CONTOUR_PACKAGE_VERSION} ..."
-# https://tanzucommunityedition.io/docs/v0.12/package-readme-contour-1.20.1/
-tanzu package available list contour.community.tanzu.vmware.com
-cat <<EOF >contour-values.yaml
-envoy:
-  service:
-    type: ClusterIP
-  hostPorts:
-    enable: true
-EOF
-tanzu package install contour \
-  --package-name contour.community.tanzu.vmware.com \
-  --version ${CONTOUR_PACKAGE_VERSION} \
-  --values-file contour-values.yaml
-
-<<com
-#Validate Package is Running
-PNAME="projectcontour"
-PACKAGE="contour"
-APPNAME=$(kubectl -n $PNAME get po -l app=$PACKAGE -o name | cut -d '/' -f 2)
-echo "> Validating $PNAME is ready..."
-for index in "${APPNAME[@]}"
-do
-        CSTATUS='NotRunning'
-        while [[ $CSTATUS != "Running" ]]
-        do
-                echo "${APPNAME[index]} - NotRunning"
-                CSTATUS=$(kubectl get po -n $PNAME | grep ${APPNAME[index]} | awk '{print $3}')
-        done
-echo "   ${APPNAME[index]} - $CSTATUS"
-done
-com
-
-#Validate Tanzu Package is reconciled
-PNAME="projectcontour"
-PACKAGE="contour"
-CSTATUS='NotReconziled'
-echo "   Validating $PNAME is reconciled ..."
-while [[ $CSTATUS != "Reconcile succeeded" ]]
-do
-echo "$PNAME not reconciled"
-CSTATUS=$(tanzu package installed get $PACKAGE | grep STATUS | awk '{print $2" "$3}')
-sleep 5s
-done
-echo "$PNAME $CSTATUS"
-sleep 20s
-
 ## Install Pi-Hole
 echo "   Adding helm repo mojo2600 for pihole deployment ..."
 #Reference https://greg.jeanmart.me/2020/04/13/self-host-pi-hole-on-kubernetes-and-block-ad/
 helm repo add mojo2600 https://mojo2600.github.io/pihole-kubernetes/
 helm repo update
-
-## Download pihole yaml file from registry
-# Download Repo Folder
-echo -e "\e[92m  Downloading YAML Repo Folder ..." > /dev/console
-git clone --filter=blob:none --sparse ${REPO}/${REPONAME}
-cd ${REPONAME}
-git sparse-checkout init --cone
-git sparse-checkout add ${REPOFOLDER}
-cd ${REPOFOLDER}
-mv pihole.values.yaml /${USERD}/automation/.
-echo -e "\e[92m  Cleaning up YAML Repo Folder ..." > /dev/console
-rm -rf *
-rm -rf .*
-cd ..
-rmdir ${REPOFOLDER}
-# clean up Repo Folder
-rm -rf *
-rm -rf .*
-cd ..
-rmdir ${REPONAME}
 
 # Create Namespace pihole
 echo "   Creating Namespace pihole ..."
@@ -167,55 +67,51 @@ echo " "
 
 # Get Timezone from OVA
 TZ=$(timedatectl status | grep "Time zone" | cut -d ":" -f 2 | cut -d " " -f 2)
-echo "   System Timezone is $TZ ..."
-echo "   Updating yaml with System Timezone ..."
-sudo sed -i "s#UTC#$TZ#g" /${USERD}/automation/pihole.values.yaml
+echo "   Applying system timezone $TZ for pihole ..."
 
 # Install Pihole
 echo "   Using helm to install pihole ..."
 helm install pihole mojo2600/pihole \
   --namespace pihole \
-  --values pihole.values.yaml \
-  --replace 
+  --replace \
+  --set image.tag="latest" \
+  --set extraEnvVars.TZ="${TZ}" \
+  --set hostNetwork="true" \
+  --set serviceDns.type="ClusterIP" \
+  --set serviceDhcp.type="ClusterIP" \
+  --set serviceWeb.type="ClusterIP" \
+  --set virtualHost="pihole" \
+  --set ingress.enabled="false" \
+  --set ingress.hosts[0]="$(hostname -d)" \
+  --set persistentVolumeClaim.enabled="true" \
+  --set admin.existingSecret="pihole-secret" \
+  --set doh.enabled="false" \
+  --set hostname="pihole" \
+  --set dnsHostPort.enabled="true" \
+  --set serviceDns.mixedService="true" \
+  --set subdomain="$(hostname -d)"
 
 # Validate that pihole pod is ready
 echo "   Validate that pihole pod is ready ..."
-PIHOLEPOD=$(kubectl get po -n pihole | grep pihole | cut -d " " -f 1)
-while [[ $(kubectl get po -n pihole $PIHOLEPOD -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "   Waiting for pod $PIHOLEPOD to be ready ..." && sleep 10s; done
+PIHOLEPOD=$(kubectl get pods --namespace pihole -l "app.kubernetes.io/name=pihole,app.kubernetes.io/instance=pihole" -o jsonpath="{.items[0].metadata.name}")
+while [[ $(kubectl get po -n pihole $PIHOLEPOD -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "Waiting for pod $PIHOLEPOD to be ready" && sleep 1; done
 echo "   Pod $PIHOLEPOD is now ready ..."
 
-# Echo pod info
-echo "   Info on new pod includes:"
+# Show pihole pod info
+echo "   Pihole pod info: ..."
 kubectl get pods -n pihole -o wide
-kubectl get pvc -n pihole
 kubectl get services -n pihole -o wide
-echo " "
+kubectl describe services -n pihole
+kubectl get ingress -n pihole -A
+#kubectl logs ${PIHOLEPOD} -n pihole --all-containers
 
-# Create Pihole ingress rules
-echo "   Creating pihole ingress rules ..."
-cat <<EOF > ingress.yaml
-apiVersion: networking.k8s.io/v1
-kind: Ingress
-metadata:
-  name: pihole-web
-  namespace: pihole
-spec:
-  rules:
-  - host: $PIHOLE_FQDN
-    http:
-      paths:
-      - pathType: Prefix
-        path: /
-        backend:
-          service:
-            name: pihole-web
-            port:
-              number: 80
-EOF
-
-# Apply Pihole ingress rules
-echo "   Applying pihole ingress rules ..."
-kubectl apply -f ingress.yaml
+# Setup Pihole for local DNS resolution
+#echo "   Setting Pihole DNS resolution for local PhotonOS ..."
+#sudo unlink /etc/resolv.conf
+#NODE_IP=$(kubectl get node ${CONTROL_PLANE} -o yaml | grep 'projectcalico.org/IPv4Address:' | cut -d " " -f 6 | cut -d "/" -f 1)
+#sudo echo "nameserver ${NODE_IP}" | sudo tee /etc/resolv.conf
+#sudo systemctl enable systemd-resolved
+#sudo systemctl start systemd-resolved
 
 # Echo Completion
 sleep 10s
